af30e2d7995e343fe31325d5b3f1a2c46c9e9532
nicoasp <nicolas.amaya@gmail.com>
2017-05-04 18:31:01 +0200

    figuring out data model for bank

--- the_bank/src/index.js
+++ the_bank/src/index.js
@@ -3,6 +3,53 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+
+
+state: {
+	accounts: [
+		{
+			id: 1,
+			amount: 1000
+		}
+	],
+	transactions: [
+		{
+			id: 1,
+			amount: 20,
+			type: "withdraw",
+			from: 2,
+			to: null
+		}
+	]
+}
+
+state: {
+	accounts: [
+		{
+			id: 1,
+			amount: 1000,
+			transactions: [
+				{
+					id: 1,
+					amount: 20,
+					type: "withdraw",
+					from: null,
+					to: null
+				}
+			]			
+		}
+	],
+
+}
+
+
+
+
+
+
+
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
