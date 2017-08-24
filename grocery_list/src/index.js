27238ea7e6657d051a4262086177c806da27645e
Alex-Willenbrink <awillenbrink@gmail.com>
2017-08-24 11:04:47 -0500

    some strange outputs for purchasing items

--- grocery_list/src/index.js
+++ grocery_list/src/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { addItem, purchaseItem } from './actions';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { addItem, purchaseItem } from "./actions";
 
+import groceryListApp from "./reducers";
+import { createStore } from "redux";
 
+const store = createStore(groceryListApp);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+console.log("initial state: ", store.getState());
+
+store.dispatch(
+  addItem({
+    name: "Apple",
+    category: "Fruit",
+    amount: 5
+  })
+);
+
+setTimeout(() => {
+  store.dispatch(purchaseItem(1));
+}, 5000);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
--- grocery_list/src/reducers/index.js
+++ grocery_list/src/reducers/index.js
@@ -1,35 +1,38 @@
-import { ACTION_ADD_ITEM, ACTION_PURCHASE_ITEM } from './actions';
+import { ACTION_ADD_ITEM, ACTION_PURCHASE_ITEM } from "../actions";
 
 /*
  * items
  * id, name, amount, category, purchased
  */
 
-
 const initialState = {
   items: []
-}
+};
 
-const groceryListApp(state = initialState, action) {
+const groceryListApp = (state = initialState, action) => {
   switch (action.type) {
     case ACTION_ADD_ITEM:
       return {
         ...state,
-        items: [
-          ...state.items,
-          action.data
-        ]
-      }
-    break;
+        items: [...state.items, action.data]
+      };
+
     case ACTION_PURCHASE_ITEM:
-      const item = state.items.find(item => item.id === action.data.id);
-      if (!item) return state;
-      item.purchased = true;
+      const items = state.items.map(item => {
+        if (item.id === action.data) {
+          item.purchased = true;
+        }
+        return item;
+      });
+
       return {
         ...state,
+        items
+      };
 
-      }
-    break;
-
+    default:
+      return state;
   }
-}
+};
+
+export default groceryListApp;
