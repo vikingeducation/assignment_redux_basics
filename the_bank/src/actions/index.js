d67e06a32d13c21f8905cc37e857d15238118497
Andrew Senner <drewdiddy611@gmail.com>
2017-08-24 18:57:40 -0400

    done

--- the_bank/src/actions/index.js
+++ the_bank/src/actions/index.js
@@ -3,34 +3,46 @@ export const ACTION_WITHDRAW = 'ACTION_WITHDRAW';
 export const ACTION_DEPOSIT = 'ACTION_DEPOSIT';
 export const ACTION_TRANSFER = 'ACTION_TRANSFER';
 
-let accountId = 0,
-	transactionId = 0;
-const incrementId = type => {
-	let id = 0;
-	switch (type) {
-		case ACTION_CREATE_ACCOUNT:
-			id = ++accountId;
-			break;
-		default:
-			id = ++transactionId;
-	}
-	return id;
-};
-
-const createAction = (type, defaults = {}) => data => {
-	defaults.id = incrementId(type);
-	defaults.date = new Date().toJSON();
-	if (type === ACTION_CREATE_ACCOUNT) defaults.transactions = [];
+const createAction = type => data => {
 	return {
 		type,
-		data: Object.assign({}, defaults, data)
+		data: Object.assign({}, data)
 	};
 };
 
-export const createAccount = createAction(ACTION_CREATE_ACCOUNT, {
-	funds: 0
-});
+// const createAction2 = (type, defaults) => data => {
+// 	defaults.id = incrementId(type);
+// 	defaults.date = new Date().toJSON();
+// 	defaults.transactions = Array.of(...defaults.transactions);
+// 	return {
+// 		type,
+// 		data: Object.assign({}, defaults, data)
+// 	};
+// }
+//
+// function createAction3(type, defaults = {}) {
+// 	defaults.id = incrementId(type);
+// 	defaults.date = new Date().toJSON();
+// 	switch (type) {
+// 		case ACTION_CREATE_ACCOUNT:
+// 			return function(data) {
+// 				defaults.transactions = [];
+// 				return {
+// 					type,
+// 					data: Object.assign({}, defaults, data)
+// 				};
+// 			};
+// 		default:
+// 			return function(data) {
+// 				return {
+// 					type,
+// 					data: Object.assign({}, defaults, data)
+// 				};
+// 			};
+// 	}
+// }
 
+export const createAccount = createAction(ACTION_CREATE_ACCOUNT);
 export const deposit = createAction(ACTION_DEPOSIT);
 export const withdraw = createAction(ACTION_WITHDRAW);
 export const transfer = createAction(ACTION_TRANSFER);
--- the_bank/src/reducers/index.js
+++ the_bank/src/reducers/index.js
@@ -1,64 +1,85 @@
 import {
-  ACTION_CREATE_ACCOUNT,
-  ACTION_WITHDRAW,
-  ACTION_DEPOSIT,
-  ACTION_TRANSFER
-} from "../actions";
+	ACTION_CREATE_ACCOUNT,
+	ACTION_WITHDRAW,
+	ACTION_DEPOSIT,
+	ACTION_TRANSFER
+} from '../actions';
 
 const initialState = {
-  accounts: []
+	accounts: []
 };
 
+let transactionId = 0;
 export default (state = initialState, action) => {
-  let accounts = state.accounts.slice();
-  switch (action.type) {
-    case ACTION_CREATE_ACCOUNT:
-      return {
-        ...state,
-        accounts: [...accounts, action.data]
-      };
-    case ACTION_WITHDRAW:
-    case ACTION_DEPOSIT:
-      accounts = accounts.map(account => {
-        if (account.id === action.data.accountId) {
-          const amount = action.data.amount;
-          account.funds += action.type === ACTION_WITHDRAW ? -amount : amount;
-          account.transactions.push(action);
-        }
-        return account;
-      });
-      return {
-        ...state,
-        accounts
-      };
-    case ACTION_TRANSFER:
-      // console.log("state.accounts: ", state.accounts);
-      // console.log("action.data: ", action.data);
-      const from = accounts.find(
-        account => account.id === action.data.fromAccountId
-      );
-      const to = accounts.find(
-        account => account.id === action.data.toAccountId
-      );
-      if (!from || !to) return state;
+	let accounts = state.accounts.slice();
+	switch (action.type) {
+		case ACTION_CREATE_ACCOUNT:
+			action.data = Object.assign(
+				{
+					id: accounts.length + 1,
+					funds: 0,
+					transactions: [],
+					date: new Date().toJSON()
+				},
+				action.data
+			);
+			return {
+				...state,
+				accounts: [...accounts, action.data]
+			};
+		case ACTION_WITHDRAW:
+		case ACTION_DEPOSIT:
+			accounts = accounts.map(account => {
+				if (account.id === action.data.accountId) {
+					const amount = action.data.amount;
+					account.funds += action.type === ACTION_WITHDRAW ? -amount : amount;
 
-      // Extract amount.
-      const amount = action.data.amount;
+					action.data = Object.assign(
+						{
+							id: ++transactionId,
+							date: new Date().toJSON()
+						},
+						action.data
+					);
+					console.log(action, 'ACTION');
+					account.transactions.push(action);
+				}
+				return account;
+			});
+			return {
+				...state,
+				accounts
+			};
+		case ACTION_TRANSFER:
+			const from = accounts.find(
+				account => account.id === action.data.fromAccountId
+			);
+			const to = accounts.find(
+				account => account.id === action.data.toAccountId
+			);
+			if (!from || !to) return state;
 
-      // Remove funds from source account.
-      from.funds -= amount;
-      from.transactions.push(action);
+			// Extract amount.
+			const amount = action.data.amount;
+			action.data.date = new Date().toJSON();
+			action.data.id = ++transactionId;
 
-      // Add funds to target account.
-      to.funds += amount;
-      to.transactions.push(action);
-      console.log(state);
+			// Remove funds from source account.
+			from.funds -= amount;
+			from.transactions.push(action);
 
-      return {
-        ...state,
-        accounts
-      };
-    default:
-      return state;
-  }
+			action.data.id = ++transactionId;
+
+			// Add funds to target account.
+			to.funds += amount;
+			to.transactions.push(action);
+			console.log(state);
+
+			return {
+				...state,
+				accounts
+			};
+		default:
+			return state;
+	}
 };
