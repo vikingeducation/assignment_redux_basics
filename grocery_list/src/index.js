1c7b6278ac1a311dcfc20ce8b1e361411d9af9ef
Alex-Willenbrink <awillenbrink@gmail.com>
2017-08-24 12:03:07 -0500

    super duper sorting is fininto

--- grocery_list/src/index.js
+++ grocery_list/src/index.js
@@ -1,71 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { addItem, purchaseItem } from './actions';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { addItem, purchaseItem } from "./actions";
 
-import groceryListApp from './reducers';
-import { createStore } from 'redux';
+import groceryListApp from "./reducers";
+import { createStore } from "redux";
 
-import faker from 'faker';
+import faker from "faker";
 
 const store = createStore(groceryListApp);
 
 let unsubscribe = store.subscribe(() => {
-	console.log(store.getState());
+  console.log(store.getState());
 });
 
-console.log('initial state: ', store.getState());
+console.log("initial state: ", store.getState());
 
 for (let i = 0; i < 1000; i++) {
-	store.dispatch(
-		addItem({
-			name: faker.commerce.productName(),
-			category: faker.commerce.department(),
-			amount: Math.floor(1 + Math.random() * 10)
-		})
-	);
-	const purchased = !!Math.round(Math.random());
-	if (purchased) {
-		store.dispatch(purchaseItem(i + 1));
-	}
+  store.dispatch(
+    addItem({
+      name: faker.commerce.productName(),
+      category: faker.commerce.department(),
+      amount: Math.floor(1 + Math.random() * 10)
+    })
+  );
+  const purchased = !!Math.round(Math.random());
+  if (purchased) {
+    store.dispatch(purchaseItem(i + 1));
+  }
 }
 
-const SORT_NAME = 'SORT_NAME';
-const SORT_DESC = 'SORT_DESC';
-const listItems = (category, filter) => {
-	const items = store
-		.getState()
-		.items.filter(
-			item =>
-				category === 'all'
-					? true
-					: item.category.toLowerCase() === category.toLowerCase()
-		)
-		.filter(filter && typeof filter === 'function' ? filter : () => true);
-	items.forEach(item => console.log(item));
-	return {
-		sort: type => {
-			if (typeof type === 'string') {
-				switch (type) {
-					case SORT_NAME:
-						break;
-					case SORT_DESC:
-						break;
-				}
-			} else if (typeof type === 'function') {
-			} else {
-				return items.sort();
-			}
-		}
-	};
+const SORT_NAME = "SORT_NAME";
+const SORT_DESC = "SORT_DESC";
+const getItems = (category = "all", filter) => {
+  let items = store.getState().items;
+
+  if (category !== "all") {
+    category = category.toLowerCase();
+    items = items.filter(item => item.category.toLowerCase() === category);
+  }
+
+  if (filter && typeof filter === "function") {
+    items = items.filter(filter);
+  }
+
+  const returnObj = {
+    print: () => items.forEach(item => console.log(item)),
+    sort: type => {
+      if (typeof type === "string") {
+        switch (type) {
+          case SORT_NAME:
+            items.sort((a, b) => b.name > a.name);
+            break;
+          case SORT_DESC:
+            items.sort((a, b) => b.name < a.name);
+            break;
+        }
+      } else if (typeof type === "function") {
+        items.sort(type);
+      } else {
+        return items.sort();
+      }
+      return returnObj;
+    }
+  };
+  return returnObj;
 };
 
-listItems('kids');
-listItems('all', el => el.amount > 5);
+getItems("kids").print();
+getItems("all", el => el.amount > 5).sort("name").print();
 
-listItems('all', el => el.purchased);
+getItems("all", el => el.purchased).print();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
