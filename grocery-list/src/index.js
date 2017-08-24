d9c2648481de1f2556794502d73d273c894d5852
Ian Halverson <halversoncode@gmail.com>
2017-08-24 11:40:39 -0400

    reducer maybe

--- grocery-list/src/index.js
+++ grocery-list/src/index.js
@@ -1,8 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { combineReducers, createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//action types
+const ADD_ITEM = `ADD_ITEM`;
+const REMOVE_ITEM = `REMOVE_ITEM`;
+const PURCHASE_ITEM = `PURCHASE_ITEM`;
+
+//actions as functions
+const purchaseItem = id => {
+  return {
+    type: _ITEM,
+    data: {
+      ...data,
+      data: id
+    }
+  };
+};
+const removeItem = id => {
+  return {
+    type: _ITEM,
+    data: {
+      ...data,
+      data: id
+    }
+  };
+};
+const addItem = id => {
+  return {
+    type: _ITEM,
+    data: {
+      ...data,
+      data: id
+    }
+  };
+};
+
+//reducers?
+function groceryApp() {}
+
+const groceryApp = combineReducers(
+  {
+    //reducers
+  }
+);
+
+const store = createStore();
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+unsubscribe();
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
