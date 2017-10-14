3a2c98cb72f00673c586f9e7be4300220a13480c
Alex <rollinglex@gmail.com>
2017-10-15 03:55:05 +0900

    added all features

--- grocery_list/src/actions.js
+++ grocery_list/src/actions.js
@@ -1,8 +1,8 @@
 // create action constants to export
 export const ADD_ITEM = "ADD_ITEM";
 export const PURCHASED = "PURCHASED";
-export const SET_FILTER = "SET_FILTER";
-export const SORT = "SORT";
+export const FILTER = "SET_FILTER";
+export const SORT_BY_NAME = "SORT_BY_NAME";
 
 //to set id numbers for items on list
 let itemId = 1;
@@ -22,15 +22,25 @@ export function addItem(data) {
 	};
 }
 //  indicate that a item has been purchased
-export function purchased() {
+export function purchased(id) {
 	return {
 		type: PURCHASED,
-		data: {}
+		data: id
+	};
+}
+//  sort by name
+export function sortByName() {
+	return {
+		type: SORT_BY_NAME
 	};
 }
 //  filter purchased/ not purchased, all, categories
-//
-//  sort by name
+export function filter(filter) {
+	return {
+		type: FILTER,
+		filter
+	};
+}
 
 //shape of grocery list object
 /*
@@ -46,6 +56,7 @@ groceries_list: [
     description: diet,
     category: fruit,
     amount: 5,
+		status: unpurchased
   }
 ]
 */
