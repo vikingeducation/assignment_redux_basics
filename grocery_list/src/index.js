7c7be4adeea2acd1094bb6c3a35ec5be70645951
Alex <rollinglex@gmail.com>
2017-10-15 23:43:30 +0900

    Revert "Revert "added all features""
    This reverts commit 6897668d23a43cca5257d7e65f9280ba3b9fe4ed.

--- grocery_list/src/index.js
+++ grocery_list/src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 import { createStore } from "redux";
 import { groceriesApp } from "./reducers";
-import { addItem } from "./actions";
+import { addItem, purchased, filter, sortByName } from "./actions";
 
 let store = createStore(groceriesApp);
 
@@ -15,35 +15,47 @@ let unsubscribe = store.subscribe(() => {
 console.log("initial state", store.getState());
 //dispatch actions
 //add new item to list with
-//  name, description, optionas(amount, category)
-store.dispatch(
-	addItem({
-		name: "apples",
-		description: "diet",
-		category: "fruit",
-		amount: 5
-	})
-);
+
 store.dispatch(
 	addItem({
 		name: "ice-cream",
 		description: "vanilla",
 		category: "snacks",
-		amount: 1
+		amount: 3,
+		status: false
 	})
 );
+//  name, description, optionas(amount, category)
 store.dispatch(
 	addItem({
 		name: "cereal",
 		description: "breakfast",
-		category: "grains",
-		amount: 2
+		category: "zgrains",
+		amount: 2,
+		status: false
+	})
+);
+
+store.dispatch(
+	addItem({
+		name: "apples",
+		description: "diet",
+		category: "fruit",
+		amount: 5,
+		status: false
 	})
 );
 //  indicate that a item has been purchased
-//
-//  filter purchased/ not purchased, all, categories
-console.log("After Dispatches", store.getState());
+console.log("BEFORE PURCHASE");
+store.dispatch(purchased(3));
+
 //  sort by name
+console.log("BEFORE SOORTTT");
+store.dispatch(sortByName());
+//  filter purchased/ not purchased, all, categories
+console.log("BEFORE set filter");
+store.dispatch(filter("purchased"));
+
+console.log("After Dispatches", store.getState());
 unsubscribe();
 ReactDOM.render(<App />, document.getElementById("root"));
--- grocery_list/src/reducers.js
+++ grocery_list/src/reducers.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 
 //import action constants from action
 
-import { ADD_ITEM } from "./actions";
+import { ADD_ITEM, PURCHASED, FILTER, SORT_BY_NAME } from "./actions";
 
 //grocerylist reducers
 //
@@ -10,26 +10,54 @@ import { ADD_ITEM } from "./actions";
 function groceryListReducer(state = [], action) {
 	switch (action.type) {
 		// adds item to list
+		case SORT_BY_NAME:
+			if (state.length < 1) {
+				return state;
+			} else {
+				state.sort((a, b) => {
+					a = a.name;
+					b = b.name;
+					return a < b ? -1 : a > b ? 1 : 0;
+				});
+			}
 		case ADD_ITEM:
 			return [...state, action.data];
 		//set purchased to true for matched item
+		case PURCHASED:
+			return state.map((item, i) => {
+				if (action.data === item.id) {
+					return {
+						...item,
+						status: true
+					};
+				} else {
+					return item;
+				}
+			});
 		default:
 			return state;
 	}
 }
 //set filter reducers
-//
+function filter(state = { filter: "all" }, action) {
+	switch (action.type) {
+		case FILTER:
+			return {
+				filter: action.filter
+			};
+		default:
+			return state;
+	}
+}
 //purchased
 //not purchased
 //all
 //categories
 
-//sortList reducers
-//
-
 //combine reducers
 export const groceriesApp = combineReducers({
-	groceryListReducer
+	groceryListReducer,
+	filter
 });
 //shape of grocery list object
 
