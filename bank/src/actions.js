876828c51e216ac564a9637e80de9bbeede6e7a8
Lakshmi <lakshmi.nishthala@gmail.com>
2017-12-21 15:23:27 -0800

    Trying to merge

--- bank/src/actions.js
+++ bank/src/actions.js
@@ -1,6 +1,6 @@
 export const CREATE_ACCOUNT = "CREATE_ACCOUNT";
 export const VIEW_ACCOUNT = "VIEW_ACCOUNT";
-export const DEPOSIT_ACCOUNT = "VIEW_ACCOUNT";
+export const DEPOSIT_ACCOUNT = "DEPOSIT_ACCOUNT";
 
 let nextAccountId = 1;
 export function createAccount(data) {
