59e135a0715fd4d84062936135ac5b9438d9c83e
Jeff Dederick <Xpressivmotion@gmail.com>
2017-12-21 12:28:27 -0500

    set reducers

--- src/reducers.js
+++ src/reducers.js
@@ -8,4 +8,39 @@ import {
   SET_SORT_GROCERY
 } from './actions';
 
-function
+function groceries(state = [], action) {
+  switch (action.type) {
+    case ADD_GROCERY:
+      return [...state, action.data];
+    case PURCHASE_GROCERY:
+      return state.map(item => {
+        if (item.id === action.data) {
+          return {
+            ...item,
+            purchased: true
+          };
+        }
+        return item;
+      });
+    case UPDATE_GROCERY:
+      return state.map(item => {
+        if (item.id === action.data.id) {
+          return action.data;
+        }
+        return item;
+      });
+    default:
+      return state;
+  }
+}
+
+function groceryFilters(state = 'NOT_PURCHASED', action) {
+  switch (action.type) {
+    case SET_PURCHASED_FILTER:
+      return action.data;
+    default:
+      return state;
+  }
+}
+
+export const groceryApp = combineReducers({ groceries, groceryFilters });
